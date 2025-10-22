2a478c1b1d61a2e3936a7840989cbf3b521acd62
adesh.exc <bodead@rknec.edu>
2025-10-22 20:51:07 +0530

    Validate parameters in clone function & minor correctn
    Added validation for 'url' and 'branch' parameters in clone function.

--- Jenkins-shared-library/vars/clone.groovy
+++ Jenkins-shared-library/vars/clone.groovy
@@ -1,5 +1,11 @@
 def call(String url , String branch ){
+
+  if (!url?.trim() || !branch?.trim()) {
+        error "Both 'url' and 'branch' parameters must be provided!"
+  }
+  
   echo 'Hello Project - clone called'
-  git branch: branch, url: branch
+  git branch: branch, url: url
   echo 'Clone github Project through git plugin & custom shared library call named clone.groovy'
+  
 }
